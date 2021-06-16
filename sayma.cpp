cd9d7abe5ad7b5aab689c7024f75a4e5e93e4930
Cihanpym <75257977+Cihanpym@users.noreply.github.com>
2021-06-16 21:41:23 +0300

    Create sayma.cpp

---
+++ sayma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int sayi;
+    cout << "Sayı Giriniz : ";
+    cin >> sayi;
+    for (int i = 1; i <= sayi; i++) {
+        cout << i << endl;
+    }
+}
